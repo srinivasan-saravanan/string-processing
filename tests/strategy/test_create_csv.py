3342dc8188933ec1bde6f231fa054997bbe5a14d
srinivasan-saravanan <89300262+srinivasan-saravanan@users.noreply.github.com>
2022-02-13 23:40:58 +0530

    Update test_create_csv.py
    added test cases for create_csv

--- tests/strategy/test_create_csv.py
+++ tests/strategy/test_create_csv.py
@@ -1,1 +1,13 @@
+import pytest
+import os
+from src.strategy.create_csv import CreateCSV
+
+
+class TestCreateCSV(object):
+    @pytest.mark.parametrize("word, file_path", [("hello world", "output.csv")])
+    @pytest.mark.process_string
+    def test_process_string(self, word, file_path):
+        CreateCSV(word).process_string()
+        if not os.path.isfile(file_path):
+            raise Exception("File not exists")
 
