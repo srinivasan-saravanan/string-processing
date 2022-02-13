55d3abd27f0379356f286a8bba050c0a53c52300
srinivasan-saravanan <89300262+srinivasan-saravanan@users.noreply.github.com>
2022-02-13 16:01:10 +0530

    modified the test_app file

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,19 +2,6 @@ import pytest
 from src.app import StringToUpper, StringToAlternateUpper, CreateCSV
 
 
-class TestStringToUpper(object):
-
-    @pytest.mark.parametrize("word, expected", [("hello world", "HELLO WORLD")])
-    @pytest.mark.process_string
-    def test_process_string(self, word, expected):
-        result = StringToUpper(word).process_string()
-        assert result == expected
 
 
-class TestStringToAlternateUpper(object):
 
-    @pytest.mark.parametrize("word, expected", [("hello world", "HELLO WORLD")])
-    @pytest.mark.process_string
-    def test_process_string(self, word, expected):
-        result = StringToAlternateUpper(word).process_string()
-        assert result == expected
